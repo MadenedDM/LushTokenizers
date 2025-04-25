0c1e18eaee8dd9d5ff8de7832d199a0066e815aa
MadenedDM <119270374+MadenedDM@users.noreply.github.com>
2025-04-25 12:04:09 -0500

    added functions, strings, and chars

--- python/toklushpy1.py
+++ python/toklushpy1.py
@@ -7,7 +7,46 @@ from typing import Any, Generator, Optional
 
 
 END_OF_TOKEN = {" ", "\n"}
-TOKENS = {"+", "-", "(", ")", "[", "]", ";"}  # Single Character Tokens Only
+TOKENS = {
+    "+",
+    "-",
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}",
+    ";",
+    ",",
+    ":",
+}  # Single Character Tokens Only
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "z",
+]
 
 
 class TokenEnum(StrEnum):
@@ -21,11 +60,21 @@ class TokenEnum(StrEnum):
     PAREN_LEFT = "PAREN_LEFT"  # (
     PAREN_RIGHT = "PAREN_RIGHT"  # )
 
-    STRING = "STRING"
+    BRACE_LEFT = "BRACE_LEFT"  # {
+    BRACE_RIGHT = "BRACE_RIGHT"  # }
+
+    CHAR = "CHAR"  # ''
+    STRING = "STRING"  # ""
 
     PRINT = "PRINT"  # print
+    EXIT = "EXIT"  # exit
 
     SEMICOLON = "SEMICOLON"  # ;
+    COLON = "COLON"
+    COMMA = "COMMA"  #  ,
+
+    FUNCTION = "FUNCTION"  # fn
+    REFRENECE = "REFERENCE"
 
     TYPE_DEC = "TYPE_DEC"  # Type Declarations (int32, int64, str, bool, ...)
 
@@ -48,6 +97,7 @@ class Tokenizer:
     def __init__(self, text: str):
         self.pointer = 0
         self.text = text
+        self.identifiers = set()
 
     def pull_next(self) -> Token:
         content = ""  # Content is local to each run
@@ -76,14 +126,69 @@ class Tokenizer:
             if content in TOKENS:
                 break
 
+            if content == "'":  # Chars
+                char = self.text[self.pointer]
+
+                self.pointer += 1
+
+                if self.text[self.pointer] == "'":
+                    self.pointer += 1
+                    return Token(TokenEnum.CHAR, char)
+                else:
+                    print("Error Occured While Processing:")
+                    print("Unclosed Char")
+                    exit(4)
+
+            if content == '"':  # Strings
+                string_content = ""
+
+                self.pointer -= 1
+
+                while self.pointer < len(self.text):
+                    self.pointer += 1
+                    if self.text[self.pointer] == '"':
+                        self.pointer += 1
+                        return Token(TokenEnum.STRING, f"'{string_content}'")
+                    string_content += self.text[self.pointer]
+
+                print("Error Occured While Processing:")
+                print("Unclosed String")
+                exit(3)
+
             if self.text[self.pointer] in TOKENS:
                 break
 
         match content:
             case _ if content.isdigit():
                 return Token(TokenEnum.INT, int(content))
+            case _ if content in self.identifiers:
+                return Token(TokenEnum.REFRENECE, str(content))
             case "print":
                 return Token(TokenEnum.PRINT)
+            case "exit":
+                return Token(TokenEnum.EXIT)
+            case "fn":
+                identifier = ""
+                while (self.text[self.pointer] in END_OF_TOKEN) and (
+                    self.pointer < len(self.text)
+                ):  # Whitespace trim
+                    self.pointer += 1
+
+                    if self.pointer == len(self.text):  # EOF Token
+                        print("Error Occured While Processing:")
+                        print("Undefined Funtion")
+                        exit(5)
+
+                while (
+                    (self.text[self.pointer] not in END_OF_TOKEN)
+                    and (self.pointer < len(self.text))
+                    and (self.text[self.pointer].lower() in letters)
+                ):
+                    identifier += self.text[self.pointer]
+                    self.pointer += 1
+
+                self.identifiers.add(identifier)
+                return Token(TokenEnum.FUNCTION, identifier)
             case "+":
                 return Token(TokenEnum.PLUS)
             case "-":
@@ -92,8 +197,16 @@ class Tokenizer:
                 return Token(TokenEnum.PAREN_LEFT)
             case ")":
                 return Token(TokenEnum.PAREN_RIGHT)
+            case "{":
+                return Token(TokenEnum.BRACE_LEFT)
+            case "}":
+                return Token(TokenEnum.BRACE_RIGHT)
             case ";":
                 return Token(TokenEnum.SEMICOLON)
+            case ":":
+                return Token(TokenEnum.COLON)
+            case ",":
+                return Token(TokenEnum.COMMA)
             case _:  # Unspecified token
                 print("Error Occured While Processing:")
                 print(f"Unrecognized token at character {self.pointer}: {content}")
@@ -112,11 +225,14 @@ def main(path: str):
     with open(path, mode="r") as file:
         content = file.read()
         tokenizer = Tokenizer(content)
+        tokcount = 0
 
         with open(path.strip(".lush") + ".tok", mode="w") as write_target:
             for token in tokenizer:
                 # print(token, end="")
+                tokcount += 1
                 write_target.write(str(token))
+        print(f"Total Tokens: {tokcount}")
 
 
 if __name__ == "__main__":
