070246e30e497fea15606256dcfe05702c6c60f0
MadenedDM <119270374+MadenedDM@users.noreply.github.com>
2025-04-25 16:26:48 -0500

    Wrote Some Basic tests

---
+++ tests/test_tokenizer.py
@@ -0,0 +1,41 @@
+from toklushpy1 import *
+
+
+def tok(code: str) -> str:
+    cont = ""
+
+    for token in Tokenizer(code):
+        cont += str(token)
+
+    return cont
+
+
+def test_number_tokens():
+    assert (
+        tok("1 2 30 4570 8914 ")
+        == "INT\t1;\nINT\t2;\nINT\t30;\nINT\t4570;\nINT\t8914;\nEOF;\n"
+    )
+
+
+def test_semicolon():
+    assert tok(";") == "SEMICOLON;\nEOF;\n"
+
+
+def test_colon():
+    assert tok(":") == "COLON;\nEOF;\n"
+
+
+def test_print():
+    assert tok("print ( ) ;") == "PRINT;\nPAREN_LEFT;\nPAREN_RIGHT;\nSEMICOLON;\nEOF;\n"
+    assert tok("print( );") == "PRINT;\nPAREN_LEFT;\nPAREN_RIGHT;\nSEMICOLON;\nEOF;\n"
+    assert (
+        tok("print(5);")
+        == "PRINT;\nPAREN_LEFT;\nINT\t5;\nPAREN_RIGHT;\nSEMICOLON;\nEOF;\n"
+    )
+
+
+def test_math_text():
+    assert (
+        tok("10024 + 489 - 702;")
+        == "INT\t10024;\nPLUS;\nINT\t489;\nSUB;\nINT\t702;\nSEMICOLON;\nEOF;\n"
+    )
